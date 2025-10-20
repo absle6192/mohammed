426f015d8bc3b57ec8365a11aae911bc2273fbd0
absle6192 <iabad2030@gmail.com>
2025-10-20 15:46:44 +0300

    Fix FastAPI import and define basic routes

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-
+from from
+fastapi import FastAPI
+fastapi.responses import JSONResponse, F
+app = FastAPI()
+@app.get("/")
+def home():
+return HTMLResponse("<h3>Trading bot serv
+@app.get ("/health")
+def health():
+return
+JSONResponse ({"status": "ok"})
