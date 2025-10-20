a527427686fccc0d00578fdd9b76721953c6f8ad
absle6192 <iabad2030@gmail.com>
2025-10-20 15:31:09 +0300

    delete app.py content

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse, HTMLResponse
 
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return HTMLResponse("<h3>Trading bot service is up ✅</h3>")
-
-@app.get("/health")
-def health():
-    return JSONResponse({"status": "ok"})
