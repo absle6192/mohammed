46df505ed1d52f1b0d0eb5dfcace2f34dc782aef
absle6192 <iabad2030@gmail.com>
2026-03-15 02:33:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+last_price = None
+
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     data = {
@@ -14,19 +16,47 @@ def send_telegram(message):
     requests.post(url, json=data)
 
 def get_price():
-    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-    response = requests.get(url)
-    data = response.json()
-    return data["price"]
+    url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=NQ=F"
+    data = requests.get(url).json()
+    price = data["quoteResponse"]["result"][0]["regularMarketPrice"]
+    return price
 
 while True:
     try:
+        global last_price
+
         price = get_price()
-        message = f"💰 BTC Price: {price}"
-        print(message)
-        send_telegram(message)
+
+        if last_price is not None:
+
+            if price > last_price:
+
+                message = f"""
+📈 BUY NQ
+
+Entry: {price}
+SL: {price - 20}
+TP: {price + 40}
+"""
+
+                send_telegram(message)
+
+            elif price < last_price:
+
+                message = f"""
+📉 SELL NQ
+
+Entry: {price}
+SL: {price + 20}
+TP: {price - 40}
+"""
+
+                send_telegram(message)
+
+        last_price = price
+
         time.sleep(60)
 
     except Exception as e:
-        print("❌ Error:", e)
+        print("Error:", e)
         time.sleep(10)
