750494499d428f38c0979644f27d526e8795deb8
absle6192 <iabad2030@gmail.com>
2026-03-15 02:21:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, json={"chat_id": CHAT_ID, "text": message})
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    requests.post(url, json=data)
 
 def get_price():
-    url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=NQ=F"
-    data = requests.get(url).json()
-    return data["quoteResponse"]["result"][0]["regularMarketPrice"]
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    response = requests.get(url)
+    data = response.json()
+    return data["price"]
 
 while True:
     try:
         price = get_price()
-        message = f"NQ Price: {price}"
+        message = f"💰 BTC Price: {price}"
         print(message)
         send_telegram(message)
         time.sleep(60)
+
     except Exception as e:
-        print("Error:", e)
+        print("❌ Error:", e)
         time.sleep(10)
